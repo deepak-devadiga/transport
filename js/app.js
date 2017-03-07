c168a9ef62a874712cd91d81cc89e21975743ac6
Deepak K Bhaskar <deepucsait@gmail.com>
2017-03-07 17:24:03 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -40,7 +40,7 @@ $(document).load(function() {
             $('#page1').fadeIn(1000).show();
             $('#footer').fadeIn(1000).show();
 
-            return false; //This doesn't prevent the form from submitting.
+            return false; 
         }
     });
 
