95fcb140b94031f023ae02c73391133ff574ce17
Deepak K Bhaskar <deepucsait@gmail.com>
2017-03-08 13:22:40 +0530

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -12,7 +12,9 @@
     app.controller('TransportDetails', function($scope) {
         console.log("Transport Details Initialized");
         this.detail = {};
-		
+		if(this.detail.box > 0){
+			this.cartoon = true;
+		}
         this.addTransDetails = function() {
 			transport_list.push(this.detail);
 			localStorage.setItem('transport_DB', JSON.stringify(transport_list));
@@ -31,6 +33,16 @@
             }
 
         }
+		
+		this.disable = function(item, e){
+			if(item >0  && e == 1){
+				this.disable_cartoon = true;
+			}else if(item > 0 && e == 2){
+				this.disable_box = true;
+			}
+		}
+		
+		
     });
 	
 	
@@ -40,4 +52,31 @@
         this.trans_lists = transport_list;
         console.log(this.trans_list);
     });
+	
+	
+	app.controller('ModuleSelect', function(){
+		console.log("Module Select Initialized");
+		this.selectMod = function(opt){
+			$('#transport').removeClass('active1');
+			$('#commission').removeClass('active1');
+			if(opt == 1){
+				$('#transport').addClass('active1');
+			}else{
+				$('#commission').addClass('active1');
+			}
+		}
+		this.nextPage = function(){
+			$('#transport').css('filter','grayscale(100%)');
+			$('#commission').css('filter','grayscale(100%)');
+			$('#page').css('filter','grayscale(100%)');
+				$('#transport').css('cursor','default');
+				$('#commission').css('cursor','default');
+				document.getElementById('transport').style.pointerEvents = 'none';
+				document.getElementById('commission').style.pointerEvents = 'none';
+				$('#page_button').prop('disabled',true);
+				$('#page1').fadeIn(1000).show();
+				
+		}
+		
+	});
 })();
