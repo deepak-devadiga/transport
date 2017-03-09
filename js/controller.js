cb59d6edefae6eb0e327957d0502e850754dedd5
Deepak K Bhaskar <deepucsait@gmail.com>
2017-03-09 10:03:59 +0530

    Update controller.js

--- js/controller.js
+++ js/controller.js
@@ -86,7 +86,7 @@
 		this.editPage = function(){
 			$('#page_edit').fadeIn(200).hide(500);
 			$('#page_edit').css('filter','grayscale(0)');
-			$('#page_button').fadeOut(200).show(500);
+			$('#page_button').fadeIn(200).show(500);
 			$('#transport').css('border','none');
 			$('#commission').css('border','none');
 			$('#transport').css('filter','grayscale(0)');
